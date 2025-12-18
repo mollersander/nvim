43686ec4504d5e8441ac8fd46548abf742167b0a
Sander Möller <69638788+mollersander@users.noreply.github.com>
2025-12-18 22:28:36 +0200

    fix window/pane transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,15 @@
 -- colorscheme
 vim.g.gruvbox_material_enable_italic = true
-vim.g.gruvbox_matrial_enable_bold = true
-vim.g.gruvbox_matrial_ui_contrast = "high"
+vim.g.gruvbox_material_enable_bold = true
+vim.g.gruvbox_material_ui_contrast = "high"
 vim.g.gruvbox_material_background = "hard"
 vim.cmd.colorscheme("gruvbox-material")
--- vim.cmd(":hi statusline guibg=none")
--- vim.api.nvim_set_hl(0, "VertSplit", { fg = "#5e81ac", bg = "NONE" })
--- vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE", underline = true })
 
--- Override background to follow terminal style
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal",   { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" , fg = nil})
+  end,
+})
+vim.cmd.colorscheme(vim.g.colors_name or "default")
 
