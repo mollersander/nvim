be29f38c0ea424625e4061399516aba702c31557
Sander Möller <69638788+mollersander@users.noreply.github.com>
2025-12-19 18:33:08 +0200

    add cursor smear

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -31,6 +31,7 @@ vim.pack.add({
   { src = "https://github.com/christoomey/vim-tmux-navigator" },
   { src = "https://github.com/kylechui/nvim-surround" },
   { src = "https://github.com/karb94/neoscroll.nvim" },
+  { src = "https://github.com/sphamba/smear-cursor.nvim" },
 
   -- Git
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
---
+++ lua/plugins/smearcursor.lua
@@ -0,0 +1,10 @@
+require("smear_cursor").setup({
+  stiffness = 0.5,
+  trailing_stiffness = 0.5,
+  matrix_pixel_threshold = 0.5,
+  stiffness_insert_mode = 0.7,          
+  trailing_stiffness_insert_mode = 0.7, 
+  damping = 0.95,                       
+  damping_insert_mode = 0.95,           
+  distance_stop_animating = 0.5,
+})
